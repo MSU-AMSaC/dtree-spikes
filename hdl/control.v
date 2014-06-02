9f6e1ecb49865b7598901b244857ddc02ef45872
Sam Boling <charles.samuel.boling@gmail.com>
2014-06-02 11:55:32 -0400

    refactors in control

--- hdl/control.v
+++ hdl/control.v
@@ -41,7 +41,7 @@ module control
   
   output wire[$clog2(FEATURES)-1 : 0] level;
   output wire[$clog2(FEATURES)-1 : 0] path;
-  output reg                          out_valid;
+  output wire                         out_valid;
   
   /* module body */
   reg [ 2                            /* indicates whether the child is present */
@@ -95,33 +95,36 @@ module control
                 -: BIAS_BIT_DEPTH ]
  
   localparam TREE_HEIGHT = $clog2(MAX_CLUSTERS);
-  reg [TREE_HEIGHT-1        : 0] node_index  = 0;
+  reg [TREE_HEIGHT-1        : 0] node_index;
   reg [$clog2(FEATURES-1)-1 : 0] coeff_index = 0;
     
-  localparam STATE_READ   = 0
+  localparam STATE_DECIDE = 0
            , STATE_INDEX  = 1
-           , STATE_DECIDE = 2
-           , STATE_DONE   = 3
            ;
 
-  reg [$clog2(STATE_DONE)-1 : 0] state = STATE_READ;
+  reg [$clog2(STATE_INDEX)-1 : 0] state = STATE_DECIDE;
 
   reg [1                  : 0] child_flags      = 0;
-  reg [BIAS_BIT_DEPTH-1   : 0] bias_i           = 0;
-  reg [COEFF_BIT_DEPTH-1  : 0] coeff_i          = 0;
+  reg                          child_valid;
+
+  reg [BIAS_BIT_DEPTH-1     : 0] bias_i           = 0;
+  reg [COEFF_BIT_DEPTH-1    : 0] coeff_i          = 0;
   reg [$clog2(FEATURES+1)-1 : 0] feature_counter  = 0;
-  reg [TREE_HEIGHT-1      : 0] decision_counter = 0;
+  reg [TREE_HEIGHT-1        : 0] decision_counter = 0;
+  reg [TREE_HEIGHT-1        : 0] final_depth      = 0;
  
   reg  [FEATURES-1             : 0] is_one_shr  = 0;
   reg  [FEATURES-1             : 0] path_i      = 0;
+  reg                               done        = 1'b0;
   wire [FEATURES-1             : 0] stored_one_pos;
 
   assign coeff      = coeff_i;
   assign is_one     = is_one_shr[FEATURES-1];
   assign bias       = bias_i;  
 
-  assign level      = decision_counter;
+  assign level      = final_depth;
   assign path       = path_i;
+  assign out_valid  = done;
 
   assign stored_one_pos = `lookup_one_pos(node_index);
 
@@ -129,46 +132,48 @@ module control
     begin
       if (reset == 1'b1)
         begin
-          state           <= STATE_READ;
-            mult      = 1'b0;
-          node_index      <= 0;
-          coeff_index     <= 0;
+          done  <= 1'b0;
+          state <= STATE_DECIDE;
 
-          feature_counter <= 0;
-          decision_counter   <= 0;
+          coeff_index      <= 0;
+
+          feature_counter  <= 0;
+          decision_counter <= 0;
+  
+          final_depth      <= 0;
         end
       else
         begin
           case (state) 
-            STATE_READ:
+            STATE_DECIDE:
               begin
+                state <= STATE_INDEX;
+                if (child_valid == 1'b1)
+                  begin
+                    decision_counter <= decision_counter + 1;
+                    done  <= 1'b0;
+                  end
+                else
+                  begin
+                    final_depth      <= decision_counter;
+                    decision_counter <= 0;
+                    done  <= 1'b1;
+                  end
+
+                path_i[decision_counter] <= child_direction;
+
                 child_flags <= `lookup_child_flags(node_index);
                 bias_i      <= `lookup_bias(node_index);
                 is_one_shr  <= stored_one_pos;
-                      if (child_direction == 1'b0)
-                        begin
-                          state <= STATE_DONE;
-                        end
-                      else
-                        begin
-                          state <= STATE_READ;
-                        end
                 coeff_i     <= `lookup_coeff(node_index, coeff_index);
 
-                if (stored_one_pos[FEATURES-1] == 1'b1)
-                  begin
-                    coeff_index <= coeff_index;
-                  end 
-                else
-                  begin 
-                    coeff_index <= coeff_index + 1;
-                  end
-                feature_counter <= feature_counter + 1;
-
-                state <= STATE_INDEX;
+                feature_counter <= 1;
+                coeff_index     <= 0;
               end
             STATE_INDEX:
               begin
+                done  <= 1'b0;
+
                 child_flags <= child_flags;
                 bias_i      <= bias_i;
                 is_one_shr[FEATURES-1 : 1] <= is_one_shr[FEATURES-2 : 0];
@@ -188,123 +193,67 @@ module control
                     state           <= STATE_INDEX;
                   end
               end
-            STATE_DECIDE:
-              begin
-                case (child_flags)
-                  2'b00: /* leaf */
-                    begin
-                      state <= STATE_DONE;
-                      
-                    end
-                  2'b01: /* right child only */
-                    begin
-                      if (child_direction == 1'b0)
-                        begin
-                          state <= STATE_DONE;
-                        end
-                      else
-                        begin
-                          state <= STATE_READ;
-                          decision_counter <= decision_counter + 1;
-                        end
-                    end
-                  2'b10: /* left child only */
-                    begin
-                      if (child_direction == 1'b1)
-                        begin
-                          state <= STATE_DONE;
-                        end
-                      else
-                        begin
-                          state <= STATE_READ;
-                          decision_counter <= decision_counter + 1;
-                        end
-                    end
-                  2'b11: /* both children present */
-                    begin
-                      state <= STATE_READ;
-                      decision_counter <= decision_counter + 1;
-                    end
-                endcase
-
-                path_i[decision_counter] <= child_direction;
-
-                /* Note that this logic is only valid for a tree height of 3 */
-                if (decision_counter == 0)
-                  begin
-                    node_index <= 1 + child_direction;
-                  end 
-                else
-                  begin
-                    node_index <= 3 + path_i[decision_counter-1];
-                  end
-                coeff_index <= 0;
-              end
-            STATE_DONE:
-              begin
-                state <= STATE_READ;
-                node_index       <= 0;
-                coeff_index      <= 0;
-                decision_counter <= 0;
-              end
-            default: 
-              begin
-                state <= STATE_READ;
-                node_index       <= 0;
-                coeff_index      <= 0;
-                decision_counter <= 0;
-              end
           endcase
         end
     end
 
     always @(reset, state,
-             coeff_index)
+             is_one_shr, coeff_i, 
+             feature_counter, decision_counter,
+             child_direction, child_flags, path_i)
       begin
         if (reset == 1'b1)
           begin
-            out_valid = 1'b0;
-
             load_bias = 1'b0;
             add       = 1'b0;
             mult      = 1'b0;
+
+            child_valid = 1'b0;
+            node_index  = 0;
           end
         else
           begin
             case (state)
-              STATE_READ:
+              STATE_DECIDE:
                 begin
-                  out_valid = 1'b0;
-
                   load_bias = 1'b0;
                   add       = 1'b0;
                   mult      = 1'b0;
+
+                  case (child_flags)
+                    2'b00:   child_valid = 1'b0;
+                    2'b10:   child_valid = (child_direction == 1'b0);
+                    2'b01:   child_valid = (child_direction == 1'b1);
+                    2'b11:   child_valid = 1'b1;
+                    default: child_valid = 1'b0;
+                  endcase
+
+                  if (child_valid == 1'b1)
+                    begin
+                      if (decision_counter == 0)
+                        begin
+                          node_index <= 1 + child_direction;
+                        end 
+                      else
+                        begin
+                          node_index <= 3 + path_i[decision_counter-1];
+                        end
+                    end
+                  else
+                    begin
+                      node_index <= 0;
+                    end
                 end
               STATE_INDEX:
                 begin
-                  out_valid = 1'b0;
-
                   load_bias = (feature_counter == 1) 
                             ? 1'b1
                             : 1'b0;
                   add       = 1'b1;
                   mult      = ~(is_one_shr[FEATURES-1] | (coeff_i == 0));
-                end
-              STATE_DECIDE:
-                begin
-                  out_valid = 1'b0;
 
-                  load_bias = 1'b0;
-                  add       = 1'b0;
-                  mult      = 1'b0;
-                end
-              STATE_DONE:
-                begin
-                  out_valid = 1'b1;
-
-                  load_bias = 1'b0;
-                  add       = 1'b0;
-                  mult      = 1'b0;
+                  child_valid = 1'b0;
+                  node_index = 0;
                 end
             endcase
           end
